4b723eaed60eeb75b7edd145c03975a239fa7343
RusNB <balushkin-007@mail.ru>
2024-10-06 20:28:24 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@
 public class Main {
     public static void main(String[] args) {
         int initiflBalance = 150; // начальный счет
+
         int addent = 1400; // сумма пополнения для бонуса
 
         int bonus = 0;
+
         if (addent > 1000) {
             bonus = addent / 100;
             int finalBalance = initiflBalance + addent + bonus;
             System.out.println("Итоговый баланс:" + finalBalance);
 
-        }else {
-            System.out.println(  " Всего на счету" );
+        } else {
+            System.out.println(" Всего на счету");
         }
 
 
